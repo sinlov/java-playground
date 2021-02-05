4e6a2a87ffbeb9c5a0958ebe90507591eca0621c
sinlov <sinlovgmppt@gmail.com>
2021-02-05 14:56:52 +0800

    add sleep to LockUnSafeDemo

--- playground/src/main/java/com/sinlov/java/playground/thread/LockSafeDemo.java
+++ playground/src/main/java/com/sinlov/java/playground/thread/LockSafeDemo.java
@@ -2,13 +2,18 @@ package com.sinlov.java.playground.thread;
 
 public class LockSafeDemo implements Runnable {
 
-    public static final int COUNT = 10000;
+    public static final int COUNT = 100;
     private static int a = 0;
 
     @Override
     public synchronized void run() {
-        for (int i = 0; i < COUNT; i++) {
-            a++;
+        try {
+            for (int i = 0; i < COUNT; i++) {
+                Thread.sleep(1);
+                a++;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
--- playground/src/main/java/com/sinlov/java/playground/thread/LockUnSafeDemo.java
+++ playground/src/main/java/com/sinlov/java/playground/thread/LockUnSafeDemo.java
@@ -2,13 +2,18 @@ package com.sinlov.java.playground.thread;
 
 public class LockUnSafeDemo implements Runnable {
 
-    public static final int COUNT = 10000;
+    public static final int COUNT = 100;
     private static int a = 0;
 
     @Override
     public void run() {
-        for (int i = 0; i < COUNT; i++) {
-            a++;
+        try {
+            for (int i = 0; i < COUNT; i++) {
+                Thread.sleep(1);
+                a++;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
