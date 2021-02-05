1146a77f3a14bde0416e639c1e7a2ad25aee9b06
sinlov <sinlovgmppt@gmail.com>
2021-02-05 14:59:31 +0800

    try to LockUnSafeDemo more count

--- playground/src/main/java/com/sinlov/java/playground/thread/LockUnSafeDemo.java
+++ playground/src/main/java/com/sinlov/java/playground/thread/LockUnSafeDemo.java
@@ -2,18 +2,13 @@ package com.sinlov.java.playground.thread;
 
 public class LockUnSafeDemo implements Runnable {
 
-    public static final int COUNT = 100;
+    public static final int COUNT = 100000;
     private static int a = 0;
 
     @Override
     public void run() {
-        try {
-            for (int i = 0; i < COUNT; i++) {
-                Thread.sleep(1);
-                a++;
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < COUNT; i++) {
+            a++;
         }
     }
 
