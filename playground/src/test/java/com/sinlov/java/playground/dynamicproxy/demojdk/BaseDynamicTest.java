831d9830dc0116c302d96e8a728cf7ab518d2dee
sinlov <sinlovgmppt@gmail.com>
2021-02-08 19:40:46 +0800

    let BaseDynamicTest.java write simple

--- playground/src/test/java/com/sinlov/java/playground/dynamicproxy/demojdk/BaseDynamicTest.java
+++ playground/src/test/java/com/sinlov/java/playground/dynamicproxy/demojdk/BaseDynamicTest.java
@@ -5,7 +5,6 @@ import test.TempTest;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 import static org.junit.Assert.*;
 
@@ -13,18 +12,18 @@ public class BaseDynamicTest extends TempTest {
     @Test
     public void test_dynamic_proxy() {
         BaseDynamicProxy base = new BaseDynamicProxy();
-
-        BaseDynamicInterface proxyInterface = (BaseDynamicInterface) Proxy.newProxyInstance(base.getClass().getClassLoader(), base.getClass().getInterfaces(), new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                if (method.getName().equals("sayString")) {
-                    return "say proxy something";
-                }
-                return method.invoke(base, args);
-            }
-        });
         assertEquals("do say string", base.sayString());
-        base.getProxy(new InvocationHandler() {
+//        BaseDynamicInterface proxyInterface = (BaseDynamicInterface) Proxy.newProxyInstance(base.getClass().getClassLoader(), base.getClass().getInterfaces(), new InvocationHandler() {
+//            @Override
+//            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//                if (method.getName().equals("sayString")) {
+//                    return "say proxy something";
+//                }
+//                return method.invoke(base, args);
+//            }
+//        });
+//        assertEquals("say proxy something", proxyInterface.sayString());
+        BaseDynamicInterface proxy = base.getProxy(new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 if (method.getName().equals("sayString")) {
@@ -33,6 +32,7 @@ public class BaseDynamicTest extends TempTest {
                 return method.invoke(base, args);
             }
         });
-        assertEquals("say proxy something", proxyInterface.sayString());
+
+        assertEquals("say proxy something", proxy.sayString());
     }
 }
